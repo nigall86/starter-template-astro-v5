424a94b3652ccba665b3095a3d8826dade98aa0e
Gennadiy <nigall86@gmail.com>
2023-12-08 16:08:12 +0300

    December upgrade

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,22 +1,11 @@
-const postcssPresetEnv = require('postcss-preset-env');
-const postcssImport = require('postcss-import');
-const postcssCustomMedia = require('postcss-custom-media');
-const postcssGlobalData = require('@csstools/postcss-global-data');
-const cssnano = require('cssnano');
-
-// const lib = process.env.npm_lifecycle_event;
-
-// const inlineMediaQueries = lib === 'lib:media' || lib === 'lib:supports';
-// todo: inline MQs for 'lib:all' when it's supported better
-
 module.exports = {
   plugins: [
-    postcssGlobalData({
-      files: ['./src/sass/abstracts/_media-queries.css'],
-    }),
-    postcssCustomMedia(),
-    cssnano({
-      preset: 'default',
+    require('postcss-import'),
+    require('autoprefixer'),
+    require('postcss-preset-env')({
+      features: {
+        'nesting-rules': true,
+      },
     }),
   ],
 };
---
+++ src/content/config.ts
@@ -0,0 +1,46 @@
+import { z, defineCollection, reference } from 'astro:content';
+
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string().max(60, {
+      message: 'Title must be 60 characters or less.',
+    }),
+    description: z.string().max(160, {
+      message: 'Description must be 160 characters or less.',
+    }),
+    date: z.date(),
+    author: reference('team'),
+    relatedPosts: z.array(reference('posts')).optional(),
+  }),
+});
+
+const teamsCollection = defineCollection({
+  type: 'data',
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      bio: z.string(),
+      email: z.string(),
+      role: z.enum(['Software', 'Design', 'Marketing']),
+      headshot: image(),
+    }),
+});
+
+const servicesCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    contact: reference('team'),
+    available: z.boolean(),
+    tags: z.array(z.enum(['b2b', 'b2c', 'saas', 'ecommerce'])),
+  }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+  team: teamsCollection,
+  services: servicesCollection,
+};
