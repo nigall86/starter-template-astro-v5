a656f5b7a84e95d54634b2affd50c73099e5dedd
Gennadii Antonov <nigall86@gmail.com>
2024-02-10 17:48:04 +0300

    New Astro, change to CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
+// https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
       external: ['svgo'],
     },
   },
+  integrations: [icon()],
+  devToolbar: { enabled: false },
 });
