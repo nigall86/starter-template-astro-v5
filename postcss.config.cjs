b2f036c3f17028b2456908ff174cc4a07bb303fd
Gennadii Antonov <nigall86@gmail.com>
2024-02-15 21:30:37 +0300

    Huge upgrade, cards component and many more

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-nested'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       features: {
