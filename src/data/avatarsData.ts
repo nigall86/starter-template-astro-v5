1ba0773bf9a7cc131bbd63649e5799a308dd238f
Gennadii Antonov <nigall86@gmail.com>
2024-10-10 19:45:41 +0300

    Improvements + Avatars component

---
+++ src/data/avatarsData.ts
@@ -0,0 +1,14 @@
+export const avatars = [
+  {
+    path: '/src/content/team/carol-whitt.jpg',
+    name: 'Carol Whitt',
+  },
+  {
+    path: '/src/content/team/james-lin.jpg',
+    name: 'James Lin',
+  },
+  {
+    path: '/src/content/team/jim-karnath.jpg',
+    name: 'Jim Karnath',
+  },
+];
