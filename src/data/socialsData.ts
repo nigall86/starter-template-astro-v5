525c20bcd0aee38bb56b887322577904487006c3
Gennadiy <nigall86@gmail.com>
2025-11-25 12:13:23 +0300

    Some changes

--- src/data/socialsData.ts
+++ src/data/socialsData.ts
@@ -1,28 +1,34 @@
 export const socialLinks = [
   {
-    title: 'dribbble',
-    href: 'https://dribbble.com/',
-    icon: 'dribbble',
+    title: "dribbble",
+    href: "https://dribbble.com/",
+    icon: "dribbble",
+    color: "#ea4c89",
   },
 
   {
-    title: 'instagram',
-    href: 'https://instagram.com/',
-    icon: 'instagram',
+    title: "instagram",
+    href: "https://instagram.com/",
+    icon: "instagram",
+    color:
+      "radial-gradient(circle farthest-corner at 28% 100%, #fcdf8f 0%, #fbd377 10%, #fa8e37 22%, #f73344 35%, transparent 65%), linear-gradient(145deg, #3051f1 10%, #c92bb7 70%);",
   },
   {
-    title: 'twitter',
-    href: 'https://twitter.com/',
-    icon: 'twitter',
+    title: "twitter",
+    href: "https://twitter.com/",
+    icon: "twitter",
+    color: "black",
   },
   {
-    title: 'youtube',
-    href: 'https://youtube.com/',
-    icon: 'youtube',
+    title: "youtube",
+    href: "https://youtube.com/",
+    icon: "youtube",
+    color: "#ff0000",
   },
   {
-    title: 'github',
-    href: 'https://github.com/',
-    icon: 'github',
+    title: "github",
+    href: "https://github.com/",
+    icon: "github",
+    color: "#6e5494",
   },
 ];
